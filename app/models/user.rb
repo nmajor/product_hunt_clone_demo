b83802b7a6c97594f1be6d4230cf1dc9799bb8c6
Nicholas Major <nick@nmajor.com>
2017-03-07 23:56:06 +0000

    Forcing travis rebuild

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def full_name
+    
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
 end
